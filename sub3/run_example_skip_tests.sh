cf4338525a175a822a97c37ee845e3ecbd27cc36
sihyunwon <162791339+sihyunwon@users.noreply.github.com>
2024-09-09 20:35:51 +0900

    Add files via upload

---
+++ sub3/run_example_skip_tests.sh
@@ -0,0 +1,20 @@
+### train task#1 ###
+sh run_train.sh --in_file_trn_dialog ./data/task1.ddata.wst.txt --in_file_tst_dialog ./data/cl_eval_task1.wst.dev --model_path ./model
+### train task#2 ###
+sh run_train.sh --in_file_trn_dialog ./data/task2.ddata.wst.txt --in_file_tst_dialog ./data/cl_eval_task2.wst.dev --model_path ./model --model_file gAIa-final.pt
+### train task#3 ###
+sh run_train.sh --in_file_trn_dialog ./data/task3.ddata.wst.txt --in_file_tst_dialog ./data/cl_eval_task3.wst.dev --model_path ./model --model_file gAIa-final.pt
+### train task#4 ###
+sh run_train.sh --in_file_trn_dialog ./data/task4.ddata.wst.txt --in_file_tst_dialog ./data/cl_eval_task4.wst.dev --model_path ./model --model_file gAIa-final.pt
+### train task#5 ###
+sh run_train.sh --in_file_trn_dialog ./data/task5.ddata.wst.txt --in_file_tst_dialog ./data/cl_eval_task5.wst.dev --model_path ./model --model_file gAIa-final.pt
+### train task#6 ###
+sh run_train.sh --in_file_trn_dialog ./data/task6.ddata.wst.txt --in_file_tst_dialog ./data/cl_eval_task6.wst.dev --model_path ./model --model_file gAIa-final.pt
+### test ###
+sh run_test.sh --in_file_tst_dialog ./data/cl_eval_task1.wst.dev --model_path ./model
+sh run_test.sh --in_file_tst_dialog ./data/cl_eval_task2.wst.dev --model_path ./model
+sh run_test.sh --in_file_tst_dialog ./data/cl_eval_task3.wst.dev --model_path ./model
+sh run_test.sh --in_file_tst_dialog ./data/cl_eval_task4.wst.dev --model_path ./model
+sh run_test.sh --in_file_tst_dialog ./data/cl_eval_task5.wst.dev --model_path ./model
+sh run_test.sh --in_file_tst_dialog ./data/cl_eval_task6.wst.dev --model_path ./model
+
